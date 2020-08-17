b632e9e3eb36cbb71c96a60d533cca977382110d
Andre Duarte <andreduarte@MacBookdeAndre.home>
2020-08-17 03:10:41 +0100

    Fixed minor bugs

--- src/Modular/hex.js
+++ src/Modular/hex.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 const hex = (props) =>{
     return(
-        <div onClick={(event)=>event.stopPropagation()}>
-            <h2 className={'text preventselection'} style={{color: props.textColor}}>The current color is:</h2>
-            <h1 className={'text'} style={{color: props.textColor}}>{props.hexColor}</h1>
-        </div>
+        <>
+            <div>
+                <h2 className={'text preventselection'} style={{color: props.textColor}}>The current color is:</h2>
+            </div>
+            <div onClick={(event)=>event.stopPropagation()}>
+                <h1 className={'text'} style={{color: props.textColor}}>{props.hexColor}</h1>
+            </div>
+        </>
     )
 }
 
